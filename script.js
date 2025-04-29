954d15c0599b8a934fd00251e6d755a5d450b2be
Zerker <84744670+Zerker-TF@users.noreply.github.com>
2025-04-29 12:20:26 -0300

    added player choice

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 
 
 console.log("Hello World");
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*3);
+    switch(choice){
+        case 0:
+            console.log("Rock");
+            break
+        case 1:
+            console.log("Paper");
+            break
+        case 2:
+            console.log("Scissors");
+            break
+        default:
+            console.log("Invalid choice");
+            break
+    }
+}
+
+function getHumanChoice(){
+    let HChoice = prompt("Choose Rock,Paper or Scissors");
+
+    switch (HChoice){
+        case 'rock':
+            console.log("Rock");
+            break
+        case 'paper':
+            console.log("Paper");
+            break
+        case 'scissors':
+            console.log("Scissors");
+            break
+    }
+}
+
+getComputerChoice();
+getHumanChoice();
